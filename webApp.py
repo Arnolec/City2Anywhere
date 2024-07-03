ff98fd9d1dda71b9e3f0aa15911f507f39c012aa
ArnoLec <arno.lecrivain@insa-rennes.fr>
2024-07-03 11:35:22 +0200

    App Web avec marker fonctionnelle, un click sur la map à côté d'une gare affiche toutes les destinations

--- webApp.py
+++ webApp.py
@@ -6,36 +6,46 @@ from streamlit_folium import st_folium
 import streamlit as st
 
 
+
 def get_pos(lat, lng):
     return lat, lng
 
 def print_map(lat, lon):
     Analyzer = Ana.AnalyzerGTFS(lat,lon,'20240601','20240731')
     Destinations = Analyzer.all()
-    #map = fl.Map(location=[lat, lon], zoom_start=7)
-    feature_group = fl.FeatureGroup("Markers")
+    st.session_state["markers"] = []
+    st.session_state["markers"].append(fl.Marker([lat, lon], popup="You are here", color = "red"))
 
-    feature_group.add_child(fl.Marker([lat, lon], popup="You are here"))
-    #fl.CircleMarker([lat, lon], radius = 2, popup=lat+lon, color = "red").add_to(map)
 
     for row in Destinations.itertuples():
-        feature_group.add_child(fl.Marker([float(row.stop_lat), float(row.stop_lon)], popup=row.stop_name))
-        print(row.stop_name, row.stop_lat, row.stop_lon)
-        #fl.CircleMarker([float(row.stop_lat), float(row.stop_lon)], radius = 2, popup=row.stop_name).add_to(map)
-    return feature_group
+        st.session_state["markers"].append(fl.Marker([float(row.stop_lat), float(row.stop_lon)], popup=row.stop_name, color = "blue"))
+        print(row.stop_name)
 
-m = fl.Map()
 
+
+if 'center' not in st.session_state:
+    st.session_state.center = [46.903354, 1.888334]
+if 'zoom' not in st.session_state:
+    st.session_state.zoom = 5
+if 'markers' not in st.session_state:
+    st.session_state.markers = []
 
 fg = fl.FeatureGroup("Markers")
+for marker in st.session_state["markers"]:
+    fg.add_child(marker)
 
-fg.add_child(fl.Marker(location=[45.5236, -122.6750], popup='Marker 1'))
-fg.add_child(fl.Marker(location=[45.5318, -122.6748], popup='Marker 2'))
+m = fl.Map(location=[46.9,1.89], zoom_start=9)
 
-m.add_child(fg)
 
 data = None
-map = st_folium(m, height=500, width=1000, key = 'new' , feature_group_to_add=fg)
+map = st_folium(
+    m,
+    center=st.session_state["center"],
+    zoom=st.session_state["zoom"],
+    height=500,
+    width=1000,
+    key = 'new',
+    feature_group_to_add=fg)
 
 
 if map.get("last_clicked"):
@@ -43,7 +53,6 @@ if map.get("last_clicked"):
 
 if data is not None:
     fg = print_map(data[0], data[1])
-    print(fg)
     st.write(data) # Writes to the app
     print(data) # Writes to terminal
 
