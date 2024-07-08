0901920c600eb380ebca3c242f8036b53a17c085
ArnoLec <arno.lecrivain@insa-rennes.fr>
2024-07-08 16:48:44 +0200

    Ajout d'une période pour le séjour, ville sélectionnée par une selectbox et quelques ajustements pour plus de propreté

---
+++ AnalyzerGTFS.py
@@ -0,0 +1,103 @@
+# Version Stable de l'AnalyzerGTFS
+import pandas as pd
+import numpy as np
+from datetime import datetime
+from datetime import timedelta
+
+# Exemple de ville : StopArea:OCE87586545
+
+class AnalyzerGTFS:
+    stop_time_trip = [] # Contient StopPoints, Trajets et horaires des trajets
+    trip = [] # Contient les trip_id, route_id et les services_id
+    stops_trip_id = [] # Contient tous les stops des trajets corrects
+    date = [] # Contient tous les services et toutes les dates comprises entre date_min et date_max, (Utile pour afficher à l'utilisateur toutes les dates possibles pour le trajet)
+    destinations = [] # Contient les stops après la ville (StopSequence > StopSequence de la ville)
+    stops = []
+    calendar_dates = []
+    routes = []
+    stop_times = []
+    trips = []
+
+    def __init__(self, lat = 46.7, lon = 1.9, date_min= datetime.today,date_max = datetime.today, path ='TER'):
+        self.calendar_dates = pd.read_csv('Data/'+ path +'/calendar_dates.txt')
+        self.stop_times = pd.read_csv('Data/'+ path +'/stop_times.txt')[['trip_id', 'stop_id', 'departure_time']]
+        self.stops = pd.read_csv('Data/'+ path +'/stops.txt')[['stop_id', 'stop_name', 'stop_lat', 'stop_lon', 'parent_station']]
+        self.trips = pd.read_csv('Data/'+ path +'/trips.txt')[['service_id', 'trip_id', 'route_id']]
+        self.id_ville = self.villes_proches(lat,lon)
+        self.calendar_dates.date = pd.to_datetime(self.calendar_dates["date"], format='%Y%m%d').dt.date
+        self.date_min = datetime.strptime(date_min, '%Y%m%d').date()
+        self.date_max = datetime.strptime(date_max, '%Y%m%d').date()
+        self.stops['parent_station'] = self.stops['parent_station'].fillna('')
+    
+    # Etape 1 : Récupérer les stops de la ville / StopArea
+    def get_stops(self):
+        ville_Set = set(self.id_ville['stop_id'].array)
+        return self.stops[[set([l]).issubset(ville_Set) for l in self.stops.parent_station.values.tolist()]]['stop_id']
+    
+    # Etape 2 : Récupérer tous les trajets des différents StopPoints, peut importe la date
+    def get_trips(self):
+        stops_ids = self.get_stops()
+        self.stop_time_trip = pd.merge(self.stop_times, stops_ids, on='stop_id')
+        self.stop_time_trip = self.stop_time_trip.drop_duplicates(subset='trip_id') #Doublons possibles si deux StopPoints différents sont sur un même trajet
+        return self.stop_time_trip['trip_id']
+
+    # Etape 3 : Récupérer les services_id des Trajets
+    def get_service_id(self):
+        trip_ids = self.get_trips()
+        self.trip = pd.merge(trip_ids, self.trips, on='trip_id')
+        return self.trip['service_id']
+    
+    # Etape 4 : Récupérer les services fonctionnant sur les dates données
+    def get_dates(self):
+        service_id = self.get_service_id()
+        dates = pd.merge(service_id, self.calendar_dates, on='service_id')
+        return dates[(dates['date'] >= self.date_min) & (dates['date'] <= self.date_max)]
+    
+    # Etape 5 : Récupérer les trajets des services fonctionnant sur les dates données
+    def get_trajets(self):
+        services_uniques = self.get_dates().drop_duplicates(subset='service_id')
+        return pd.merge(services_uniques, self.trip, on='service_id')
+    
+    # Etape 6 : Récupérer les stops des trajets corrects
+    def get_stops_trajets(self):
+        trajets = self.get_trajets()
+        return pd.merge(trajets, self.stop_times, on='trip_id')
+    
+    # Etape 7 : Récupérer les ids des destinations, c'est à dire les stops après la ville (StopSequence > StopSequence de la ville)
+    def get_stops_destinations(self):
+        stops_trip_id = self.get_stops_trajets().assign(TempsVille = "", stop_id_ville = "")
+        stop_ids_ville = self.stop_time_trip['stop_id']
+        stop_ids_ville.index = self.stop_time_trip['trip_id']
+        stop_id_ville_origine = stop_ids_ville.loc[stops_trip_id['trip_id']]
+        stops_trip_id['stop_id_ville'] = stop_id_ville_origine.array
+        time = self.stop_time_trip['departure_time'] #StopTime trip
+        time.index = self.stop_time_trip['trip_id']
+        value = time.loc[stops_trip_id['trip_id']]
+        stops_trip_id['TempsVille'] = value.array
+        self.destinations = stops_trip_id[stops_trip_id['departure_time'] > stops_trip_id['TempsVille']]
+        return self.destinations['stop_id']
+    
+    # Etape 8 : Récupérer les destinations
+    def get_destinations(self):
+        destinations_uniques = self.get_stops_destinations()
+        destinations_StopPoint = pd.merge(destinations_uniques, self.stops, on='stop_id')
+        destinationSet = set(destinations_StopPoint['parent_station'].array)
+        destinations_StopArea = self.stops[[set([l]).issubset(destinationSet) for l in self.stops.stop_id.values.tolist()]]
+        destinations_StopArea.drop_duplicates(subset='stop_id')
+        return destinations_StopArea
+    
+    # Retourne les stopArea proche du point de départ
+    def villes_proches(self,lat,long):
+        return self.stops[(self.stops['stop_lat'] > lat-0.03) & (self.stops['stop_lat'] < lat+0.03) & (self.stops['stop_lon'] > long-0.03) & (self.stops['stop_lon'] < long+0.03) & self.stops['stop_id'].str.contains('StopArea')]
+    
+    # Pas très efficace, à améliorer
+    def trajet_destination(self,villeDestination): 
+        destinationsParentStation = pd.merge(self.destinations, self.stops, on='stop_id') # Merge entre les destinations et les stops pour récupérer les parent_station
+        StopPoints = destinationsParentStation[villeDestination == destinationsParentStation['parent_station']] # Récupérer les StopPoints de la ville de destination
+        trajets = pd.merge(StopPoints, self.destinations, on=['stop_id','service_id']) # Merge entre StopPoints de la ville et les destinations pour récupérer les trajets faisant le lien entre les deux
+        trajets_services = pd.merge(trajets.drop_duplicates(subset='service_id'), self.get_dates(), on='service_id') 
+        return trajets_services # Retourne tous les trajets faisant le lien entre la ville et la destination marchant sur les critères sélectionnés
+
+    def list_of_cities(path):
+        stops = pd.read_csv('Data/'+ path +'/stops.txt')[['stop_id', 'stop_name', 'stop_lat', 'stop_lon', 'parent_station']]
+        return stops[stops['stop_id'].str.contains('StopArea')][['stop_name','stop_lat','stop_lon','stop_id']]
--- webApp.py
+++ webApp.py
@@ -1,35 +1,52 @@
 # webApp.py
 import folium as fl
-import import_ipynb
-import AnalyzerGTFS as Ana # type: ignore
+from AnalyzerGTFS import AnalyzerGTFS as Ana
 from streamlit_folium import st_folium
 import streamlit as st
 import pandas as pd
+import datetime
 
 
 
 def get_pos(lat, lng):
     return lat, lng
 
-def print_map(lat, lon):
-    AnalyzerTER = Ana.AnalyzerGTFS(lat,lon,'20240601','20240731','TER')
-    DestinationsTER = AnalyzerTER.get_destinations()
-    AnalyzerTGV = Ana.AnalyzerGTFS(lat,lon,'20240601','20240731','TGV')
-    DestinationsTGV = AnalyzerTGV.get_destinations()
-    AnalyzerINTERCITE= Ana.AnalyzerGTFS(lat,lon,'20240601','20240731','INTERCITE')
-    DestinationsINTERCITE = AnalyzerINTERCITE.get_destinations()
+def get_cities():
+    cities = {}
+    cities_TER = Ana.list_of_cities('TER') 
+    cities_TGV = Ana.list_of_cities('TGV') 
+    cities_INTERCITE = Ana.list_of_cities('INTERCITE') 
+    cities_concat = pd.concat([cities_TER, cities_TGV, cities_INTERCITE])
+    cities_concat = cities_concat.drop_duplicates(subset=['stop_id'])
+
+    for row in cities_concat.itertuples():
+        #cities.append((row.stop_name, row.stop_lat, row.stop_lon))
+        cities[row.stop_name] = (row.stop_lat, row.stop_lon,row.stop_id)
+    return cities
+    
+
+def print_map(lat, lon, periode):
+    date_min = periode[0].strftime('%Y%m%d')
+    date_max = periode[1].strftime('%Y%m%d')
+
+    analyzer_TER = Ana(lat, lon, date_min, date_max, path ='TER' )
+    destinations_TER = analyzer_TER.get_destinations()
+    
+    analyzer_TGV = Ana(lat, lon, date_min, date_max, path = 'TGV')
+    destinations_TGV = analyzer_TGV.get_destinations()
+
+    analyzer_INTERCITE= Ana(lat, lon, date_min, date_max, path = 'INTERCITE')
+    destinations_INTERCITE = analyzer_INTERCITE.get_destinations()
+
     st.session_state["markers"] = []
     st.session_state["markers"].append(fl.Marker([lat, lon], popup="You are here", color = "red"))
 
-
-    for row in DestinationsTER.itertuples():
+    for row in destinations_TER.itertuples():
         st.session_state["markers"].append(fl.Marker([float(row.stop_lat), float(row.stop_lon)], popup=row.stop_name, color = "green"))
-    for row in DestinationsTGV.itertuples():
+    for row in destinations_TGV.itertuples():
         st.session_state["markers"].append(fl.Marker([float(row.stop_lat), float(row.stop_lon)], popup=row.stop_name, color = "yellow"))
-    for row in DestinationsINTERCITE.itertuples():
+    for row in destinations_INTERCITE.itertuples():
         st.session_state["markers"].append(fl.Marker([float(row.stop_lat), float(row.stop_lon)], popup=row.stop_name, color = "black"))
-
-
 
 if 'center' not in st.session_state:
     st.session_state.center = [46.903354, 1.888334]
@@ -38,14 +55,39 @@ if 'zoom' not in st.session_state:
 if 'markers' not in st.session_state:
     st.session_state.markers = []
 
+if 'cities' not in st.session_state:
+    st.session_state.cities = get_cities()
+
+# Permet d'éviter des actualisations inutiles si la ville n'a pas changé
+if 'previous_city' not in st.session_state:
+    st.session_state.previous_city = None
+
+city_selected = st.selectbox(
+    "Sélectionnez une ville :", 
+    st.session_state.cities.keys()
+    )
+
+
+data = None
+
+today = datetime.datetime.now()
+next_year = today.year + 1
+date = st.date_input('Période envisagée pour le séjour :', (today, today), min_value=today, max_value=datetime.datetime(next_year, today.month, today.day))
+
+if (city_selected is not None) and (city_selected != st.session_state.previous_city):
+    st.session_state.center = [st.session_state.cities[city_selected][0], st.session_state.cities[city_selected][1]]
+    st.session_state.zoom = 7
+    st.session_state.previous_city = city_selected
+    if len(date) == 2:
+        print_map(st.session_state.cities[city_selected][0], st.session_state.cities[city_selected][1], date)
+
+
 fg = fl.FeatureGroup("Markers")
 for marker in st.session_state["markers"]:
     fg.add_child(marker)
 
 m = fl.Map(location=[46.9,1.89], zoom_start=9)
 
-
-data = None
 map = st_folium(
     m,
     center=st.session_state["center"],
@@ -60,7 +102,7 @@ if map.get("last_clicked"):
     data = get_pos(map["last_clicked"]["lat"], map["last_clicked"]["lng"])
 
 if data is not None:
-    fg = print_map(data[0], data[1])
+    fg = print_map(data[0], data[1], date)
     st.write(data) # Writes to the app
     print(data) # Writes to terminal
 
