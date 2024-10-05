8478b06681ec6fb90c967fe364e1cdaa8bb59db6
ArnoLec <arno.lecrivain@insa-rennes.fr>
2024-10-05 15:36:24 +0200

    test pour déploiement sur minikube

--- back_api.py
+++ back_api.py
@@ -12,16 +12,10 @@ app = FastAPI()
 
 # Nécessite une V2 pour régler probablement les requêtes et ce qui est retourné
 
-origins = [
-    "http://127.0.0.1:8000",
-    "http://localhost:8000",
-    "http://localhost",
-    "http://localhost:8080" "http://127.0.0.1",
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
